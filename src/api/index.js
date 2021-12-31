632d99bd8bf7fed452ebac25fbe5bfd864a52bf9
Irwin Rudd <irwin.crudd@gmail.com>
2021-12-31 09:26:44 +0200

    Done

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,16 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changeableUrl);
 
     return {
       confirmed,
@@ -22,7 +27,6 @@ export const fetchData = async () => {
 export const fetchDailyData = async () => {
   try {
     const { data } = await axios.get(`${url}/daily`);
-    console.log(data);
 
     const modifiedData = data.map((dailyData) => ({
       confirmed: dailyData.confirmed.total,
@@ -33,5 +37,17 @@ export const fetchDailyData = async () => {
     return modifiedData;
   } catch (err) {
     console.log(err);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);
   }
 };
