543ed31ebbe7ebdc4dba436aae736007e02976ed
Irwin Rudd <irwin.crudd@gmail.com>
2021-12-31 08:31:13 +0200

    Added UI and api calls

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,11 @@
+import styles from "./CountryPicker.module.css";
+
+const CountryPicker = () => {
+  return (
+    <div className={styles.container}>
+      <h1>CountryPicker</h1>
+    </div>
+  );
+};
+
+export default CountryPicker;
